138e058ff7cfe31f0b216a934cb637b2b29bdb66
vipink28 <vipinkalyan28@gmail.com>
2023-07-28 11:01:38 +0530

    project initialized

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageNotFound(props) {
+    return (
+        <div>
+            <h1>Error 404 -  Page Not found</h1>
+        </div>
+    );
+}
+
+export default PageNotFound;
