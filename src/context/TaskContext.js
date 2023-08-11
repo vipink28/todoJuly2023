da79ff902d601f1b67be65d93153147b6263c8aa
vipink28 <vipinkalyan28@gmail.com>
2023-08-11 12:00:38 +0530

    latest and recent task added to create task page.

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,10 +1,13 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
 
 const TaskContext = createContext();
 
 export const TaskProvider=({children})=>{
-    const { setMessage } = useContext(AuthContext);
+    const { setMessage, user } = useContext(AuthContext);
+    const [allTasks, setAllTasks] = useState(null);
+    const [recentTasks, setRecentTasks] = useState(null);
+    const [latestTask, setLatestTask]= useState(null);
 
     //Create Task function
     const createTask = async(formData)=>{
@@ -28,9 +31,37 @@ export const TaskProvider=({children})=>{
     }
 
 
+    // get all tasks
+
+    const getAllTasks=async()=>{
+        try {
+            const response = await fetch(`http://localhost:5000/tasks?userid=${user.id}`);
+            if(response.ok){
+                const tasks = await response.json();
+                setAllTasks(tasks);
+                let recent = tasks.slice(-3);
+                setRecentTasks(recent);
+                let latest = tasks[tasks.length - 1];
+                setLatestTask(latest);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        if(user){
+            getAllTasks();
+        }        
+    }, [user])
+
+
     return (
         <TaskContext.Provider value={{
-            createTask
+            createTask,
+            latestTask,
+            allTasks,
+            recentTasks
         }}>
             {children}
         </TaskContext.Provider>
--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TaskForm from '../components/TaskForm';
+import TaskContext from '../context/TaskContext';
 
 function CreateTask(props) {
+    const { latestTask, recentTasks } = useContext(TaskContext);
     return (
         <div className="container-fluid h-100">
-        <div className="row h-100">
-          <div className="col-lg-6 d-flex align-items-center justify-content-center h-100 bg-primary text-white flex-column">
-              <div className='w-75'>
-                    <TaskForm />
-              </div>
-          </div>
-          <div className="col-lg-6 d-flex align-items-center justify-content-center h-100">  
-              <div className="card w-75 border-0 shadow-sm bg-primary">                  
-                  <div className="card-body">
-                      
-                  </div>
-              </div>
-          </div>
+            <div className="row h-100">
+                <div className="col-lg-6 d-flex align-items-center justify-content-center h-100 bg-primary text-white flex-column">
+                    <div className='w-75'>
+                        <TaskForm />
+                    </div>
+                </div>
+                <div className="col-lg-6 d-flex align-items-center flex-column justify-content-center h-100">
+                    <div className="card w-75 border-0 shadow-sm bg-primary">                        
+                            <div className="card-body text-white">
+                                <div className="d-flex align-items-center mb-4">
+                                    <h3 className='mb-0'>Latest Task</h3>
+                                    <button className='ms-auto btn btn-info'>Edit Task</button>
+                                </div>
+                                <h1>{latestTask?.title}</h1>
+                                <p>{latestTask?.description}</p>
+                                <div className="d-flex align-items-center text-warning">
+                                    <p>Modified On: {latestTask?.modifiedon}</p>
+                                    <p className='ms-auto'>Due on: {latestTask?.duedate}</p>
+                                </div>
+                            </div>
+        
+                    </div>
+
+                    <div className="card w-75 border-0 shadow-sm bg-primary mt-5">
+                        <div className="card-body text-white">
+                        <div className="d-flex align-items-center mb-4">
+                            <h3 className='mb-0'>Recent Tasks</h3>                                    
+                        </div>
+                        <div className='py-3'>
+                            {
+                                recentTasks?.map((item)=>{
+                                    return(
+                                        <div key={item.id} className='p-2 d-flex border border-info align-items-center'>
+                                            <p className='mb-0'>{item.title}</p>
+                                            <p className='mb-0 ms-auto text-warning'>
+                                                {item.duedate}
+                                            </p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     );
 }
 
