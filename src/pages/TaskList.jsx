dea9e62593ad16f07f333ac5018d8ef52d33a828
vipink28 <vipinkalyan28@gmail.com>
2023-08-18 12:28:55 +0530

    tasklist added.

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TaskContext from '../context/TaskContext';
+import { dateFormat } from '../helper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function TaskList(props) {
+    const { allTasks } = useContext(TaskContext);
+
     return (
-        <div>
-            TaskList
+        <div className='py-5 container'>
+            <div className="bg-primary p-4">
+                <table className='table table-dark'>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Duedate</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allTasks ? allTasks.map((item)=>{
+                                return <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.description}</td>
+                                    <td>{dateFormat(item.duedate)}</td>
+                                    <td>
+                                        <span className='px-2'>
+                                            <FontAwesomeIcon icon={faEye}/>
+                                        </span>
+                                        <span className='px-2'>
+                                            <FontAwesomeIcon icon={faPenToSquare}/>
+                                        </span>
+                                        <span className='px-2'>
+                                            <FontAwesomeIcon icon={faTrash}/>
+                                        </span>
+                                    </td>
+                                </tr>
+                            }):"No Data"
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
